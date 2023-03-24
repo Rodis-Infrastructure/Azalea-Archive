1d6194d9bc076e1298310ac966b1d8a9c280d3ff
Nick <james.nswork@gmail.com>
2023-03-24 13:30:19 +0000

    Renamed method

--- src/utils/RoleUtils.ts
+++ src/utils/RoleUtils.ts
@@ -1,6 +1,6 @@
 import { GuildMember } from "discord.js";
 import Config from "./Config";
 
-export function isStaff(config: Config | undefined, member: GuildMember): boolean {
+export function isGuildStaff(config: Config | undefined, member: GuildMember): boolean {
     return config?.staffRoles().some(roleId => member.roles.cache.has(roleId)) ?? false;
 }
