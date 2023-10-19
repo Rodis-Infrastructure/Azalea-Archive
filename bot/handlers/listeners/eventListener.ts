2567353e401ec2ccb174c5d2090cbe7668d33888
Nick <james.nswork@gmail.com>
2023-10-19 18:43:58 +0100

    fix(Command-Component): Add `void` as a possible return type to `execute()` functions

--- bot/handlers/listeners/eventListener.ts
+++ bot/handlers/listeners/eventListener.ts
@@ -3,4 +3,5 @@ import { Events } from "discord.js";
 export default abstract class EventListener {
     // @formatter:off
     protected constructor(public name: Events, public data?: { once: boolean }) {}
+    abstract execute(...args: never): Promise<void> | void;
 }
