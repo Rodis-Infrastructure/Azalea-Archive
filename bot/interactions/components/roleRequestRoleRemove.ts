b20197e8bc122a0616b5e60adb9fece7801797a6
Nick <59822256+Archasion@users.noreply.github.com>
2023-11-18 11:16:37 +0000

    Implement role requests (#33)

---
+++ bot/interactions/components/roleRequestRoleRemove.ts
@@ -0,0 +1,54 @@
+import { InteractionResponseType } from "@bot/types/interactions";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, EmbedBuilder, Snowflake } from "discord.js";
+import { Component } from "@bot/handlers/interactions/interaction";
+import { RegexPatterns } from "@bot/utils";
+import { runQuery } from "@database/utils";
+import Config from "@bot/utils/config";
+
+export default class RemoveRolesButton extends Component<ButtonInteraction<"cached">> {
+    constructor() {
+        super({
+            name: "role-request-role-remove",
+            defer: InteractionResponseType.Default,
+            skipInternalUsageCheck: false
+        });
+    }
+
+    async execute(interaction: ButtonInteraction<"cached">, _ephemeral: never, config: Config): Promise<void> {
+        const embed = EmbedBuilder.from(interaction.message.embeds[0]);
+        const [overviewField, userListField] = embed.data.fields!;
+
+        const userIds: Snowflake[] = userListField.value.match(RegexPatterns.Snowflake.pattern) || [];
+        const roleId: Snowflake | undefined = overviewField.value.match(RegexPatterns.Snowflake.pattern)?.[1];
+
+        if (!roleId) {
+            await interaction.reply({
+                content: `${config.emojis.error} Failed to extract the role ID from the request.`,
+                ephemeral: true
+            });
+            return;
+        }
+
+        const members = await interaction.guild.members.fetch({ user: userIds });
+        await Promise.all(members.map(member => member.roles.remove(roleId)));
+
+        // The role is not permanent
+        if (!embed.data.title?.includes("Permanent")) {
+            await runQuery(`
+                DELETE
+                FROM temporary_roles
+                WHERE request_id = ${interaction.message.id}
+            `);
+        }
+
+        const deleteBtn = new ButtonBuilder()
+            .setCustomId("delete")
+            .setLabel("Delete")
+            .setStyle(ButtonStyle.Danger);
+
+        const actionRow = new ActionRowBuilder<ButtonBuilder>()
+            .setComponents(deleteBtn);
+
+        await interaction.update({ components: [actionRow] });
+    }
+}
--- bot/utils/config.ts
+++ bot/utils/config.ts
@@ -10,6 +10,8 @@ import {
     GuildTextBasedChannel,
     MessageMentionTypes,
     ModalSubmitInteraction,
+    Role,
+    SelectMenuComponentOptionData,
     Snowflake,
     userMention
 } from "discord.js";
@@ -24,7 +26,8 @@ import {
     NotificationOptions,
     RoleInteraction,
     RolePermission,
-    RolePermissions
+    RolePermissions,
+    RoleRequests
 } from "@bot/types/config";
 
 import { AnyCommandInteraction, CustomId, InteractionResponseType } from "@bot/types/interactions";
@@ -44,6 +47,10 @@ export default class Config {
 
     get proofChannelIds(): Snowflake[] {
         return this.data.proofChannelIds ?? [];
+    }
+
+    get roleRequests(): RoleRequests | undefined {
+        return this.data.roleRequests;
     }
 
     get deleteMessageSecondsOnBan(): number {
@@ -97,6 +104,23 @@ export default class Config {
         Config.instances.set(guildId, config);
 
         return config;
+    }
+
+    /** @returns {number} The duration in milliseconds until the role is removed */
+    getTemporaryRoleDuration(roleId: Snowflake): number | undefined {
+        return this.data.roleRequests?.roles.find(role => role.roleId === roleId)?.duration;
+    }
+
+    async getRequestableRoleOptions(): Promise<SelectMenuComponentOptionData[]> {
+        const roleIds = this.data.roleRequests?.roles.map(({ roleId }) => roleId) ?? [];
+        const guild = await client.guilds.fetch(this.guildId);
+        const roles = await Promise.all(roleIds.map(roleId => guild.roles.fetch(roleId)));
+        const filteredRoles = roles.filter(Boolean) as Role[];
+
+        return filteredRoles.map(role => ({
+            label: role.name,
+            value: role.id
+        }));
     }
 
     /** @param {string} value - The custom command's choice value */
