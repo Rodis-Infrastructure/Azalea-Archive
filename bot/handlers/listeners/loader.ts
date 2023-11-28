aa0b796dfd64e1eb90086406065b88e8524e91d3
nick <59822256+Archasion@users.noreply.github.com>
2023-11-28 15:35:19 +0000

    refactor(handlers): Improve types

--- bot/handlers/listeners/loader.ts
+++ bot/handlers/listeners/loader.ts
@@ -1,12 +1,14 @@
+import { AbstractInstanceType } from "@bot/types/internals.ts";
 import { startProgressBar } from "@bot/utils";
 import { client } from "@bot/client";
 
 import path from "node:path";
 import fs from "node:fs";
+import EventListener from "@bot/handlers/listeners/eventListener.ts";
 
 export async function loadListeners(): Promise<void> {
-    const dirPath = path.resolve(__dirname, "../../listeners");
-    const paths = fs.readdirSync(dirPath);
+    const directoryPath = path.resolve(__dirname, "../../listeners");
+    const paths = fs.readdirSync(directoryPath);
     const bar = startProgressBar("Loading listeners", paths.length);
 
     for (const filename of paths) {
@@ -14,12 +16,12 @@ export async function loadListeners(): Promise<void> {
             const filepath = path.resolve(__dirname, "../../listeners", filename);
             const listenerModule = await import(filepath);
             const listenerClass = listenerModule.default;
-            const listener = new listenerClass();
+            const listener: AbstractInstanceType<typeof EventListener> = new listenerClass();
 
             if (listener.data?.once) {
-                client.once(listener.name, (...args) => listener.execute(...args));
+                client.once(listener.name, (...args: unknown[]) => listener.execute(...args));
             } else {
-                client.on(listener.name, (...args) => listener.execute(...args));
+                client.on(listener.name, (...args: unknown[]) => listener.execute(...args));
             }
 
             bar.increment({ filename });
--- bot/types/internals.ts
+++ bot/types/internals.ts
@@ -8,3 +8,5 @@ export interface RegexPattern {
 }
 
 export type ExtractFuncResult<T extends RegexPattern> = Partial<Record<T["returnedFields"][number], string>>;
+
+export type AbstractInstanceType<T> = T extends { prototype: infer U } ? U : never;
